b9ac3d5ab7689d8741d1720ef0e7b4998bf1e8ec
Alex McCaffrey <alex.mccaffrey07@gmail.com>
2021-04-04 17:12:33 -0600

    footer setup

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,43 +1,57 @@
 import { Component } from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
-import "./Footer.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope, faPhoneSquare } from "@fortawesome/free-solid-svg-icons"
+import "./Footer.css";
 
 class Footer extends Component {
   render() {
     return (
       <footer>
         <h3> &#169; Alex McCaffrey</h3>
-        <a
-          class="btn btn-dark btn-social mx-2"
-          href="https://github.com/amccaff20"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <i class="fab fa-github"></i>
-        </a>
-        <FontAwesomeIcon icon={faLinkedin}></FontAwesomeIcon>
-        <a
-          class="social-btn"
-          href="https://www.linkedin.com/in/alex-mccaffrey"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <i class="fab fa-linkedin-in"></i>
-        </a>
-        <a
-          class="btn btn-dark btn-social mx-2"
-          href="mailto:alexmccaffrey07@gmail.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {" "}
-          <i class="far fa-envelope"></i>
-        </a>
+        <section className="social-icons">
+          <a
+            class="social-btn"
+            href="https://github.com/amccaff20"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            <FontAwesomeIcon icon={faGithub} size="2x"/>
+            <i class="far fa-linkedin"></i>
+          </a>
+          <a class="btn btn-dark btn-social mx-2" href="https://github.com/amccaff20" target="_blank"><i class="fab fa-github"></i></a>
+          <a
+            class="social-btn"
+            href="https://www.linkedin.com/in/alex-mccaffrey"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            <FontAwesomeIcon icon={faLinkedin} size="2x"/>
+          </a>
+          <a
+            class="social-btn"
+            href="mailto:alexmccaffrey07@gmail.com"
+            //target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            <FontAwesomeIcon icon={faEnvelope} size="2x"/>
+          </a>
+          <a
+            class="social-btn"
+            href="tel:781-336-8196"
+            //target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            <FontAwesomeIcon icon={faPhoneSquare} size="2x"/>
+          </a>
+        </section>
       </footer>
     );
   }
 }
-
 
 export default Footer;
